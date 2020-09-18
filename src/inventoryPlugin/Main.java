51877c53c3ebe4f19f46420070899ba2f4c659ad
sciencemj <sciencemj.park@gmail.com>
2020-09-18 20:18:03 +0900

    commit

--- src/inventoryPlugin/Main.java
+++ src/inventoryPlugin/Main.java
@@ -3,14 +3,19 @@ import org.bukkit.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
+public class Main extends JavaPlugin implements Listener {
     private Inventory inv;
     @Override
     public void onEnable(){
+        this.getServer().getPluginManager().registerEvents(this,this);
         this.getCommand("changename").setExecutor(this);
         this.getCommand("inv").setExecutor(this);
         PluginDescriptionFile file = this.getDescription();
@@ -27,9 +32,19 @@ public class Main extends JavaPlugin {
                 ((Player) sender).setCustomNameVisible(true);
             }
         }else if (cmd.getName().equals("inv")){
-
+            inv = Bukkit.createInventory(null, 9, "inv");
+            inv.addItem(new ItemStack(Material.COMPASS, 1));
+            Player p = Bukkit.getPlayer(strings[0]);
+            p.openInventory(inv);
         }
         return false;
+    }
+
+    @EventHandler
+    public void onInventoryClick(InventoryDragEvent e){
+        if (e.getInventory() == inv){
+            e.setCancelled(true);
+        }
     }
 
     @Override
